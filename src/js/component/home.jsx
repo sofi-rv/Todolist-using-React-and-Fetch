a743cb349fe8f6434eccc332ab11b8a7e59cddaf
sofi-rv <128938425+sofi-rv@users.noreply.github.com>
2023-08-21 15:57:01 +0000

    cambios

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 
-const URI = "https://playground.4geeks.com/apis/fake/todos/user/soficr2"
+const URI = "https://playground.4geeks.com/apis/fake/todos/user/"
 const initialState = { label: "", done: false }
 
 //create your first component
@@ -71,7 +71,7 @@ const Home = () => {
 	//  const handleUser = (e) => {
 	//  	let user = e.target.value
 	//  	setUsername(user)
-	}
+	// }
 	let getTask = async () => {
 		try {
 
@@ -85,66 +85,66 @@ const Home = () => {
 			console.log(error)
 		}
 	}
-// 	 useEffect(() => {
-// 	 	const listLoading = async () => {
-// 	 		let response = await fetch(URI + username) //como obviamos el segundo parametro, se entiende que el método es GET
-// 	 		//response es una promesa
-// 	 		if (response.ok) {
-// 				//se realiza algo si esta dentro del status 200-299 
-// 				let objResponse = await response.json()
-// 	 			console.log("respuesta correcta", objResponse)
-// 	 			setLista(objResponse)
-// 	 		} else {
-// 	 			//error
-// 	 			console.log("ERROR")
-// 	 		}
-// 	 	}
-// 	 	listLoading()
+	// 	 useEffect(() => {
+	// 	 	const listLoading = async () => {
+	// 	 		let response = await fetch(URI + username) //como obviamos el segundo parametro, se entiende que el método es GET
+	// 	 		//response es una promesa
+	// 	 		if (response.ok) {
+	// 				//se realiza algo si esta dentro del status 200-299 
+	// 				let objResponse = await response.json()
+	// 	 			console.log("respuesta correcta", objResponse)
+	// 	 			setLista(objResponse)
+	// 	 		} else {
+	// 	 			//error
+	// 	 			console.log("ERROR")
+	// 	 		}
+	// 	 	}
+	// 	 	listLoading()
 
-// }, [username])
+	// }, [username])
 
-	 useEffect(() => { getTask() })
+	useEffect(() => { getTask() })
 
-return (
-	<>
-		<div className="container">
-			<p className="title-list d-flex justify-content-center text-danger text-opacity-25">Todos</p>
-			<ul className="list-group">
-				 {/* <li className="list-group-item text-center">
+	return (
+		<>
+			<div className="container">
+				<p className="title-list d-flex justify-content-center text-danger text-opacity-25">Todos</p>
+				<ul className="list-group">
+					{/* <li className="list-group-item text-center">
 					<input className="border" placeholder="Usuario"
 						onKeyUp={(e) => { handleUser(e) }
 						}
 					/>
 				</li>  */}
-				<li className="list-group-item">
-					<input className="form-control border-0" placeholder="Whats needs to be done?" name="label" value={tarea.label} onChange={handleChange}
-						onKeyUp={
-							(e) => { handleInput(e) }
-						} />
-				</li>
-				{
-					lista && lista.length > 0 ?
-						<>{
-							lista.map((item, index) => {
-								return <li className="list-group-item" key={index}>
-									{item.label}
-									<button className=" button btn btn-outline-light border-0" type="button" onClick={e => { deleteTask(index) }}>
-										❌
-									</button>
-								</li>
-							})
-						}</>
-						:
-						<li className="list-group-item">The list is empty</li>
-				}
-				<li className="list-group-item text-secondary">
-					{lista && lista.length > 0 ? <p className="footer"> {lista.length} item left </p> : <p>You have no tasks to do</p>}
-				</li>
+					<li className="list-group-item">
+						<input className="form-control border-0" placeholder="Whats needs to be done?" name="label" value={tarea.label} onChange={handleChange}
+							onKeyUp={
+								(e) => { handleInput(e) }
+							} />
+					</li>
+					{
+						lista && lista.length > 0 ?
+							<>{
+								lista.map((item, index) => {
+									return <li className="list-group-item" key={index}>
+										{item.label}
+										<button className=" button btn btn-outline-light border-0" type="button" onClick={e => { deleteTask(index) }}>
+											❌
+										</button>
+									</li>
+								})
+							}</>
+							:
+							<li className="list-group-item">The list is empty</li>
+					}
+					<li className="list-group-item text-secondary">
+						{lista && lista.length > 0 ? <p className="footer"> {lista.length} item left </p> : <p>You have no tasks to do</p>}
+					</li>
 
-			</ul>
-		</div>
-	</>
-);
+				</ul>
+			</div>
+		</>
+	);
 };
 
 
